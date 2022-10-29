4b4a0cfb22b571cd4b048eeb29ff43449c46a207
nusubbotin <nu.subbotin@ya.ru>
2022-10-29 10:49:23 +0300

    Инициализация проекта.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1();
+    }
+
+    private static void task1() {
+        System.out.println("Часть1. Задание 1:");
+
+    }
+}
